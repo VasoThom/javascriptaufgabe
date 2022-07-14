be9fef7b8bb8daa5ea842609d1c1dbeb1ea1e14b
Vaso <vasothom@hotmail.com>
2022-07-14 13:23:57 +0200

    1,2

---
+++ solution.js
@@ -0,0 +1,3 @@
+//1. Erstelle eine Funktion, die jeden Anfangsbuchstaben der Wörter eines Strings in einen Großbuchstaben umwandelt. Anschließend soll die Funktion den String zurückgeben.
+
+//2. Entwickle eine Funktion, die einen gegebenen Text auf maximal 30 Wörter gekürzt zurückgibt. Ist der Text länger als 30 Wörter, soll in der Ausgabe ein "..." am Ende angefügt werden.
